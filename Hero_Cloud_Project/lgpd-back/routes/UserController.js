a981b99383870766520e913f6d2d287cc67c511f
Matheus Momiy <matheus.tadao@gmail.com>
2023-12-14 12:01:46 +0900

    Alterado os arquviso User e UserController para receber arquivo

--- Hero_Cloud_Project/lgpd-back/routes/UserController.js
+++ Hero_Cloud_Project/lgpd-back/routes/UserController.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import userService from '../services/UserService.js';
+
+import multer from 'multer';
+import process from 'process';
+
 let router = express.Router();
 
-import userService from '../services/UserService.js';
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './images');
+    },
+    filename: function(req, file, callback) {
+        callback(null, req.first_name + "_" + req.body.last_name + "_" + Date.now() + file.originalname)
+    }
+});
+
+const upload = multer({storage: storage}).single('file');
 
 router.post("/addUser",async function(req, res) {
     const userModel = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
-        gender: req.body.gender
+        gender: req.body.gender,
+        profile_picture: req.file.path
     }
 
     const user = await userService.saveUser(userModel);
@@ -35,7 +50,8 @@ router.put("/updateUser/:id",async function(req, res) {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email,
-        gender: req.body.gender
+        gender: req.body.gender,
+        profile_picture: req.file.path
     }
 
     const updateUser = await userService.updateUserById(req.params.id, userModel);
